5473a0352842725035059af35743a828393b748f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-18 09:22:46 +0900

    Add url for front end

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3500",
-  "http://http://localhost:5173",
+  "http://localhost:5173",
   "http://wild-goose-chase.com",
 ];
 
