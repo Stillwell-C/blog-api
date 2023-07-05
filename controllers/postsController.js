87a6f9e0f187a0fdfc8c9844a31bbfac7e223558
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-05 18:19:55 +0900

    Use findOneAndUPdate in updatePost

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -99,63 +99,33 @@ const updatePost = async (req, res) => {
     return res.status(400).json({ message: "Post ID parameter required" });
   }
 
-  const post = await Post.findOne({ _id: req.body.id }).exec();
+  const { title, epigraph, epigraphAuthor, text } = req.body;
 
-  if (!post) return res.status(400).json({ message: "Post not found" });
+  const updatedPost = await Post.findOneAndUpdate(
+    { _id: req.body.id },
+    { title, epigraph, epigraphAuthor, text },
+    { new: true }
+  ).exec();
 
-  if (req?.body?.title) post.title = req.body.title;
-  if (req?.body?.epigraph) post.epigraph = req.body.epigraph;
-  if (req?.body?.text) post.text = req.body.text;
-  if (req?.body?.author) post.author = req.body.author;
+  if (!updatedPost) return res.status(400).json({ message: "Post not found" });
 
-  const updatedPost = await post.save();
+  // const post = await Post.findOne(
+  //   { _id: req.body.id },
+  //   { title, epigraph, epigraphAuthor, text },
+  //   { new: true }
+  // ).exec();
+
+  // if (!post) return res.status(400).json({ message: "Post not found" });
+
+  // if (req?.body?.title) post.title = req.body.title;
+  // if (req?.body?.epigraph) post.epigraph = req.body.epigraph;
+  // if (req?.body?.text) post.text = req.body.text;
+  // if (req?.body?.author) post.author = req.body.author;
+
+  // const updatedPost = await post.save();
 
   res.json({ message: `Updated post: ${updatedPost._id}` });
 };
-
-// const updatePostLike = async (req, res) => {
-//   const postId = req?.params?.id;
-
-//   const { userId, increment } = req?.body;
-
-//   if (!postId) {
-//     return res.status(400).json({ message: "Post ID parameter required" });
-//   }
-
-//   const post = await Post.findOne({ _id: postId, likedUsers: {$nin: userId}}).exec();
-
-//   if (!post) return res.status(400).json({ message: "Post not found" });
-
-//   if (!userId) {
-//     return res.status(400).json({ message: "User ID parameter required" });
-//   }
-
-//   const user = await User.findById(userId).exec();
-
-//   if (!user) res.status(400).json({ message: "Post not found" });
-
-//   //Unlike will be handled with negative number
-//   post.likes += increment;
-
-//   if (increment > 0) {
-//     post.likedUsers.push(user._id);
-//     user.likedPosts.push(post._id);
-//   } else {
-//     post.likedUsers = post.likedUsers.filter(
-//       (likedUser) => likedUser !== user._id
-//     );
-//     user.likedPosts = user.likedPosts.filter(
-//       (likedPost) => likedPost !== post._id
-//     );
-//   }
-
-//   const updatedPost = await post.save();
-//   const updatedUser = await user.save();
-
-//   res.json({
-//     message: `Updated post: ${updatedPost._id} and user: ${updatedUser._id}`,
-//   });
-// };
 
 const updatePostLike = async (req, res) => {
   const postID = req?.params?.id;
