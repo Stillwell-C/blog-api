2f8999ee6d101d0e401f2587e459ac2585e49e2f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-10 16:31:29 +0900

    Add functions for update, delete, & find user posts

--- service/post.services.js
+++ service/post.services.js
@@ -43,11 +43,44 @@ const createPost = async (postData) => {
   return Post.create(postData);
 };
 
+const findAndUpdatePost = async (postId, updatedPostData) => {
+  return Post.findOneAndUpdate(
+    { _id: postId },
+    { ...updatedPostData },
+    { new: true }
+  ).exec();
+};
+
+const findAndDeletePost = async (postId) => {
+  return Post.findByIdAndDelete(postId).exec();
+};
+
+const findUserPosts = async (page, limit) => {
+  if (page || limit) {
+    const pageInt = parseInt(page) || 1;
+    const limitInt = parseInt(limit) || 10;
+
+    const postsSkip = (pageInt - 1) * limitInt;
+
+    return Post.find({ author: userId })
+      .sort("-createdAt")
+      .limit(limitInt)
+      .skip(postsSkip)
+      .lean()
+      .exec();
+  } else {
+    return Post.find({ author: userId }).sort("-createdAt").lean().exec();
+  }
+};
+
 const exportFunctions = {
   findPost,
   findTopPosts,
   findMultiplePosts,
   createPost,
+  findAndUpdatePost,
+  findAndDeletePost,
+  findUserPosts,
 };
 
 module.exports = exportFunctions;
