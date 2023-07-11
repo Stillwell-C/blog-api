4f9cec08bad772944fb8d61682e0ad9d0c6dee6c
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-11 10:04:32 +0900

    Add functions for getAll, createNew, update User

--- service/user.services.js
+++ service/user.services.js
@@ -1,9 +1,49 @@
 const User = require("../models/User");
 
-const findUserById = (userId) => {
-  return User.findById(userId);
+const findUserById = async (userId) => {
+  return User.findById(userId).select("-password").lean().exec();
 };
 
-const exportFunctions = { findUserById };
+const findMultipleUsers = async (page, limit) => {
+  if (page || limit) {
+    const pageInt = parseInt(page) || 1;
+    const limitInt = parseInt(limit) || 10;
+
+    const postsSkip = (pageInt - 1) * limitInt;
+
+    return User.find()
+      .sort("username")
+      .limit(limitInt)
+      .skip(postsSkip)
+      .select("-password")
+      .lean()
+      .exec();
+  } else {
+    return User.find().sort("username").select("-password").lean();
+  }
+};
+
+const duplicateUserCheck = async (username) => {
+  return User.findOne({ username })
+    .collation({ locale: "en", strength: 2 })
+    .lean()
+    .exec();
+};
+
+const generateNewUser = async (newUser) => {
+  return User.create(newUser);
+};
+
+const findAndUpdateUser = async (id, updateObj) => {
+  return User.findOneAndUpdate({ _id: id }, { ...updateObj }, { new: true });
+};
+
+const exportFunctions = {
+  findUserById,
+  findMultipleUsers,
+  duplicateUserCheck,
+  generateNewUser,
+  findAndUpdateUser,
+};
 
 module.exports = exportFunctions;
