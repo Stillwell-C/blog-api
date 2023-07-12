e0e5af6e42735b439e1a60770694e746162c2e59
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-12 12:23:36 +0900

    Test get comments

--- __tests__/comments.tests.js
+++ __tests__/comments.tests.js
@@ -31,6 +31,7 @@ jest.mock("../middleware/verifyJWT", () => jest.fn());
 
 jest.mock("../service/comments.services", () => ({
   findCommentById: jest.fn(),
+  findMultipleComments: jest.fn(),
 }));
 
 describe("comments route", () => {
@@ -67,6 +68,54 @@ describe("comments route", () => {
             message: "Comment not found",
           })
           .expect(400);
+      });
+    });
+  });
+
+  describe("get comments route", () => {
+    describe("given no comment data found", () => {
+      it("will return 400", async () => {
+        commentServices.findMultipleComments.mockImplementation(
+          () => undefined
+        );
+
+        await request(app)
+          .get(`/comments/`)
+          .expect("Content-Type", /json/)
+          .expect({
+            message: "No comments found",
+          })
+          .expect(400);
+      });
+    });
+
+    describe("given comment data is found", () => {
+      it("will return 200", async () => {
+        commentServices.findMultipleComments.mockImplementation(() => [
+          mockComment,
+          mockComment,
+          mockComment,
+        ]);
+
+        await request(app)
+          .get(`/comments/`)
+          .expect("Content-Type", /json/)
+          .expect(200);
+      });
+
+      it("returns the total amount of posts found", async () => {
+        commentServices.findMultipleComments.mockImplementation(() => [
+          mockComment,
+          mockComment,
+          mockComment,
+        ]);
+
+        const { body } = await request(app)
+          .get(`/comments/`)
+          .expect("Content-Type", /json/);
+        //The function this relies on is mocked, so it will not return an accurate value here.
+        //The property should be returned, though.
+        expect(body).toHaveProperty("totalComments");
       });
     });
   });
