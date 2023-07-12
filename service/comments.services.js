0f3e9fccc77b4bda3577c4411c94ce641b4733bb
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-12 14:13:45 +0900

    Add findPostComments and createNewComment

--- service/comments.services.js
+++ service/comments.services.js
@@ -27,6 +27,66 @@ const findMultipleComments = async (page, limit) => {
   }
 };
 
-const exportFunctions = { findCommentById, findMultipleComments };
+const findUserComments = async (page, limit, userId) => {
+  if (page || limit) {
+    const pageInt = parseInt(page) || 1;
+    const limitInt = parseInt(limit) || 10;
+
+    const commentsSkip = (pageInt - 1) * limitInt;
+
+    return Comment.find({ author: userId })
+      .sort("createdAt")
+      .limit(limitInt)
+      .skip(commentsSkip)
+      .lean()
+      .populate("author", "_id username")
+      .exec();
+  } else {
+    return Comment.find({ author: userId })
+      .sort("createdAt")
+      .lean()
+      .populate("author", "_id username")
+      .exec();
+  }
+};
+
+const findPostComments = async (page, limit, postId) => {
+  if (page || limit) {
+    const pageInt = parseInt(page) || 1;
+    const limitInt = parseInt(limit) || 10;
+
+    const commentsSkip = (pageInt - 1) * limitInt;
+
+    return Comment.find({ parentPostId: postId })
+      .sort("createdAt")
+      .limit(limitInt)
+      .skip(commentsSkip)
+      .lean()
+      .populate("author", "_id username")
+      .exec();
+  } else {
+    return Comment.find({ parentPostId: postId })
+      .sort("createdAt")
+      .lean()
+      .populate("author", "_id username")
+      .exec();
+  }
+};
+
+const createNewComment = async (author, parentPostId, commentBody) => {
+  return Comment.create({
+    author,
+    parentPostId,
+    commentBody,
+  });
+};
+
+const exportFunctions = {
+  findCommentById,
+  findMultipleComments,
+  findUserComments,
+  findPostComments,
+  createNewComment,
+};
 
 module.exports = exportFunctions;
