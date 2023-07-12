35131fc837178d30bd46c95db9ce8da55e6e3d7a
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-12 15:17:55 +0900

    Add login functions

--- service/auth.services.js
+++ service/auth.services.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 const generateAccessToken = (username, roles, id) => {
   return jwt.sign(
@@ -11,17 +12,29 @@ const generateAccessToken = (username, roles, id) => {
       },
     },
     process.env.ACCESS_TOKEN_CODE,
-    { expiresIn: "10m" }
+    { expiresIn: "15m" }
   );
+};
+
+const generateRefreshToken = (username) => {
+  return jwt.sign({ username }, process.env.REFRESH_TOKEN_CODE, {
+    expiresIn: "7d",
+  });
 };
 
 const hashPassword = async (password) => {
   return bcrypt.hash(password, 10);
 };
 
+const comparePasswords = async (enteredPassword, userPassword) => {
+  return bcrypt.compare(enteredPassword, userPassword);
+};
+
 const exportFunctions = {
   generateAccessToken,
+  generateRefreshToken,
   hashPassword,
+  comparePasswords,
 };
 
 module.exports = exportFunctions;
