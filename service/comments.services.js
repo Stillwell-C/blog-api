4165bbefd3cc66ddba7ca095c36a51af618469e2
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-12 12:23:58 +0900

    Add findMultipleComments

--- service/comments.services.js
+++ service/comments.services.js
@@ -4,6 +4,29 @@ const findCommentById = async (id) => {
   return Comment.findById(id).lean().populate("author", "_id username").exec();
 };
 
-const exportFunctions = { findCommentById };
+const findMultipleComments = async (page, limit) => {
+  if (page || limit) {
+    const pageInt = parseInt(page) || 1;
+    const limitInt = parseInt(limit) || 10;
+
+    const commentsSkip = (pageInt - 1) * limitInt;
+
+    return Comment.find()
+      .sort("createdAt")
+      .limit(limitInt)
+      .skip(commentsSkip)
+      .lean()
+      .populate("author", "_id username")
+      .exec();
+  } else {
+    return Comment.find()
+      .sort("createdAt")
+      .lean()
+      .populate("author", "_id username")
+      .exec();
+  }
+};
+
+const exportFunctions = { findCommentById, findMultipleComments };
 
 module.exports = exportFunctions;
