d769ac20763b1495c05a2620b26bd7cef6b74cba
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-17 17:26:47 +0900

    Add validation to delete comment

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -152,6 +152,11 @@ const updateComment = async (req, res) => {
 };
 
 const deleteComment = async (req, res) => {
+  const valResult = validationResult(req);
+  if (!valResult.isEmpty()) {
+    return res.status(400).json({ message: "Invalid user input received" });
+  }
+
   const { id } = req.body;
 
   if (!id) {
