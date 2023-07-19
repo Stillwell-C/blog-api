9475d6b8f03419ba8ca40641ac470cc54c674b79
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-19 19:18:57 +0900

    Add cors logic check

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -5,6 +5,7 @@ const corsOptions = {
     // Will allow req from things like postman
     // May remove for production
     console.log("origin: ", origin);
+    console.log(allowedOrigins.indexOf(origin));
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
