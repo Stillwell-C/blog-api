470cd672cc60166e05f522b09e496f93af4ff902
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-19 16:55:13 +0900

    Add netlify to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://wild-goose-chase-production.up.railway.app",
   "https://www.wild-goose-chase-production.up.railway.app",
+  "https://clever-hotteok-ba90cb.netlify.app/",
 ];
 
 module.exports = allowedOrigins;
