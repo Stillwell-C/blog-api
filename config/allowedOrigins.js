e53814f3956ed2e40590ddfaf5c0d2d1605f5d56
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-19 17:39:06 +0900

    Add vercel to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://wild-goose-chase-production.up.railway.app",
   "https://www.wild-goose-chase-production.up.railway.app",
   "https://clever-hotteok-ba90cb.netlify.app/",
+  "https://wild-goose-chase.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
