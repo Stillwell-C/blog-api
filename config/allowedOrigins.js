28737a0da31b5619fd6f524a10a1eadb8ccd0d88
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-19 15:49:34 +0900

    Add gh-pages to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "https://wild-goose-chase.onrender.com",
   "https://stillwell-c.github.io/blog-frontend/",
   "https://www.stillwell-c.github.io/blog-frontend/",
+  "https://stillwell-c.github.io/",
+  "https://www.stillwell-c.github.io/",
 ];
 
 module.exports = allowedOrigins;
