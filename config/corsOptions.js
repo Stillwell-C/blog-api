ce17a8463b784dea796e88e481a1cafb0ea9aaf6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-20 14:36:20 +0900

    Remove console logs

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,8 +4,6 @@ const corsOptions = {
   origin: (origin, callback) => {
     // Will allow req from things like postman
     // May remove for production
-    console.log("origin: ", origin);
-    console.log(allowedOrigins.indexOf(origin));
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
