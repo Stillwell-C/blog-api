55573a1634335fce19f23f0a1c9d787e39c56558
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-20 14:11:37 +0900

    Remove unused code that is now in app.js

--- server.js
+++ server.js
@@ -9,32 +9,6 @@ connectDB();
 
 const app = createServer();
 
-// app.use(express.json());
-
-// app.use(cors(corsOptions));
-
-// app.use(cookieParser());
-
-// app.use("/", express.static(path.join(__dirname, "public")));
-// app.use("/", require("./routes/root"));
-// app.use("/users", require("./routes/userRoutes"));
-// app.use("/posts", require("./routes/postRoutes"));
-// app.use("/comments", require("./routes/commentRoutes"));
-// app.use("/auth", require("./routes/authRoutes"));
-
-// app.all("*", (req, res) => {
-//   res.status(404);
-//   if (req.accepts("html")) {
-//     res.sendFile(path.join(__dirname, "views", "404.html"));
-//   } else if (req.accepts("json")) {
-//     res.json({ message: "404 Not Found" });
-//   } else {
-//     res.type("txt").send("404 Not Found");
-//   }
-// });
-
-// app.use(errorHandler);
-
 mongoose.connection.once("open", () => {
   console.log("Connected to DB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
