aea6fa81ff291660bc15b5eefc347eab970bad24
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-12 08:24:52 +0900

    Update cookie properties

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,7 +42,7 @@ const login = async (req, res) => {
   res.cookie("jwt", refreshToken, {
     //Change later to https
     //Not accessible to JS
-    httpsOnly: true,
+    httpOnly: true,
     secure: true,
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
